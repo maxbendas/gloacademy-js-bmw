2a22dc149d1a87390c77c3d39c305a7a054e2afd
maxbendas <56072735+maxbendas@users.noreply.github.com>
2021-05-18 22:09:56 +0300

    day 1

---
+++ script/accordion.js
@@ -0,0 +1,21 @@
+const featureLinkElems = document.querySelectorAll('.feature__link')
+const featureSubElems = document.querySelectorAll('.feature-sub')
+
+featureLinkElems.forEach((btn, index)=>{
+    btn.addEventListener('click', ()=>{
+
+        if (btn.classList.contains('feature__link_active')){
+            featureSubElems[index].classList.add('hidden')
+            btn.classList.remove('feature__link_active')
+        }else{
+            featureSubElems.forEach((elem)=>{
+                elem.classList.add('hidden')
+            })
+            featureLinkElems.forEach((btn)=>{
+                btn.classList.remove('feature__link_active')
+            })
+            featureSubElems[index].classList.remove('hidden')
+            btn.classList.add('feature__link_active')
+        }
+    })
+})
