de22bc631d599e0645fe0cf3d448b1be465a6256
maxbendas <56072735+maxbendas@users.noreply.github.com>
2021-05-20 21:59:23 +0300

    day 2

---
+++ script/smoothScrolls.js
@@ -0,0 +1,43 @@
+const SPEED = 1
+const scrolled = event => {
+    event.preventDefault()
+    const target = event.target
+    if (target.matches('[href^="#"]')) {
+        let start = 0
+        const pageY = window.pageYOffset
+        const hash = target.getAttribute('href')
+        if (hash === '#') return
+        const elem = document.querySelector(hash)
+        const cordinateElem = elem.getBoundingClientRect().top
+        console.log(cordinateElem)
+
+        const step = time => {
+            if (!start) start = time
+            const progress = time - start
+
+            const r = (cordinateElem < 0 ?
+                Math.max(pageY - progress / SPEED, pageY + cordinateElem) :
+                Math.min(pageY + progress / SPEED, pageY + cordinateElem))
+
+            window.scrollTo(0, r)
+
+            if (r < pageY + cordinateElem) requestAnimationFrame(step)
+        }
+        requestAnimationFrame(step)
+    }
+}
+
+document.body.addEventListener('click', scrolled)
+
+
+// const smoothScrollElems = document.querySelectorAll('a[href^="#"]:not(a[href="#"])')
+//
+// smoothScrollElems.forEach(link=>{
+//     link.addEventListener('click', (e)=>{
+//         e.preventDefault()
+//         const id = link.getAttribute('href').substring(1)
+//         document.getElementById(id).scrollIntoView({
+//             behavior: "smooth"
+//         })
+//     })
+// })
